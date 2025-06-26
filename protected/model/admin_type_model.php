f7daef0fe64318564b558822a816dc9c91007ebb
Syfirry <72477786+Syfirry@users.noreply.github.com>
2025-06-26 15:22:22 -0700

    changed files

--- protected/model/admin_type_model.php
+++
@@ -1,6 +0,0 @@
-<?php
-class admin_type_model extends Model
-{
-    public $table_name = 'mannabook_admin_type';
-    
-}
--- protected/tmp/b7c4415d0e839dbc16d5ae2e41087927.1563649261.meta.html.php
+++
@@ -1,5 +0,0 @@
-<?php if(!class_exists("View", false)) exit("no direct access allowed");?><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="renderer" content="webkit|ie-comp|ie-stand">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>天糧書室管理系统</title>
-<link rel="shortcut icon" type="image/png" href="<?php echo htmlspecialchars($APP_PATH, ENT_QUOTES, "UTF-8"); ?>/i/images/icon.png" />
